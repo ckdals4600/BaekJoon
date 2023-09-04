e06f2dd802f8be568b7c001a9ff6b0547b2cd5dd
Jansoon <57699212+ckdals4600@users.noreply.github.com>
2023-09-04 17:16:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	vector<int> tile(n + 1);
+	tile[1] = 1;
+	tile[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		tile[i] = (tile[i - 1] + tile[i - 2]) % 10007;
+	}
+
+	cout << tile[n] ;
+}
