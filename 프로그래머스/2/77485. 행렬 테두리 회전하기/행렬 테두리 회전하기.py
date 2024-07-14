96029ff4447c8110337a534e8d7310f7188b3837
Jansoon <57699212+ckdals4600@users.noreply.github.com>
2024-07-14 22:13:32 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 348.71 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -7,27 +7,19 @@ def solution(rows, columns, queries):
         n = table[x1-1][y1-1]
         ret = n
         for y in range(y1,y2):
-            ex = table[x1-1][y]
-            table[x1-1][y] = n
-            n = ex
+            table[x1-1][y],n = n,table[x1-1][y]
             ret = min(n,ret)
 
         for x in range(x1,x2):
-            ex = table[x][y2-1]
-            table[x][y2-1] = n
-            n = ex
+            table[x][y2-1], n = n, table[x][y2-1]
             ret = min(n,ret)
 
         for y in range(y2-2,y1-2,-1):
-            ex = table[x2-1][y]
-            table[x2-1][y] = n
-            n = ex
+            table[x2-1][y], n = n, table[x2-1][y]
             ret = min(n,ret)
             
         for x in range(x2-2,x1-2,-1):
-            ex = table[x][y1-1]
-            table[x][y1-1] = n
-            n = ex
+            table[x][y1-1], n = n, table[x][y1-1]
             ret = min(n,ret)
         
         return ret
